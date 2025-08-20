0e4512c62ff38828c2fd9aa97d0074f216f5d88b
Jacob <jacobtimmer04@gmail.com>
2025-08-20 09:09:03 +0200

    Add FilamentLoggerPlugin & update README.md

---
+++ src/FilamentLoggerPlugin.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Z3d0X\FilamentLogger;
+
+use Filament\Contracts\Plugin;
+use Filament\Panel;
+use Z3d0X\FilamentLogger\Resources\ActivityResource;
+
+class FilamentLoggerPlugin implements Plugin
+{
+    public function getId(): string
+    {
+        return 'filament-logger';
+    }
+
+    public function register(Panel $panel): void
+    {
+        $panel->resources([
+            ActivityResource::class,
+        ]);
+    }
+
+    public function boot(Panel $panel): void
+    {
+        //
+    }
+
+    public static function make(): static
+    {
+        return app(static::class);
+    }
+
+    public static function get(): static
+    {
+        /** @var static $plugin */
+        $plugin = filament(app(static::class)->getId());
+
+        return $plugin;
+    }
+}
